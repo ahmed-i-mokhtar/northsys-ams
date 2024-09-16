09e4b0341f934fff46eecb9227a95e27cc5a5191
Ahmed Mokhtar <ai.mokht.eng@gmail.com>
2024-09-16 18:19:03 +0200

    add data apis

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 import unicorn
 from fastapi.responses import FileResponse
 from starlette.middleware.cors import CORSMiddleware
-
+import glob
 
 app = FastAPI()
 
@@ -32,16 +32,40 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.get("/image")
-async def get_image():
+@app.get("/pano_image/{location}")
+async def get_pano_image(location: str):
     # Path to the image
-    image_path = "./data/pano/zzrdBgRfm_S1ov6yLScyjw.jpg"
-
+    image_path = f"./data/server_pano_images/{location}.png"
     # Check if the file exists
     if os.path.exists(image_path):
         return FileResponse(image_path, media_type="image/jpeg")
     else:
         return {"error": "Image not found"}
+
+
+@app.get("/depth_image/{location}")
+async def get_depth_image(location: str):
+    # Path to the image
+    image_path = f"./data/server_depth_images/{location}.png"
+    # Check if the file exists
+    if os.path.exists(image_path):
+        return FileResponse(image_path, media_type="image/jpeg")
+    else:
+        return {"error": "Image not found"}
+
+
+@app.get("/get_locations")
+async def get_locations():
+
+    # get file names in the directory
+    files = os.listdir("./data/server_pano_images")
+    locations = []
+    for file in files:
+        location = file.split(".png")[0]
+        locations.append(location)
+
+    # Return  response
+    return locations
 
 
 if __name__ == "__main__":
